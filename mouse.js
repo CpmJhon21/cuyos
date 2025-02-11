78d9d089c58c494f5716f846ad180f1187620b55
deaaprizal <deascript@icloud.com>
2025-02-11 13:48:20 +0700

    remove: mouse touch styling

--- mouse.js
+++
@@ -1,8 +0,0 @@
-
-document.addEventListener("mousemove", function (cb) {
-  let x = cb.clientX
-  let y = cb.clientY
-  touchIndicator.style.left = `${x}px`
-  touchIndicator.style.top = `${y}px`
-  touchIndicator.style.transform = 'translate(-50%, -50%)'
-})
