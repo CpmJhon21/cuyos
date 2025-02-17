5822a8b56574864fa18320b4b7858211866e6003
CpmJhon21 <cpmjho@gmail.com>
2025-02-17 14:36:46 +0700

    Tes.js

--- classes/Computer/Tes.js
+++ classes/Computer/Tes.js
@@ -1,12 +1,11 @@
 function toggleOption(optionId, arrowId) {
-  let option = document.getElementById(optionId);
-  let arrow = document.getElementById(arrowId);
-
-  if (option.style.display === "none" || option.style.display === "") {
-    option.style.display = "table-row";  // Menampilkan opsi
-    arrow.innerHTML = "▲";  // Mengubah ikon jadi ▲
-  } else {
-    option.style.display = "none";  // Menyembunyikan opsi
-    arrow.innerHTML = "▼";  // Mengubah ikon jadi ▼
-  }
-}
+            let option = document.getElementById(optionId);
+            let arrow = document.getElementById(arrowId);
+            if (option.style.display === "none" || option.style.display === "") {
+                option.style.display = "table-row";
+                arrow.innerHTML = "▲";
+            } else {
+                option.style.display = "none";
+                arrow.innerHTML = "▼";
+            }
+        }
